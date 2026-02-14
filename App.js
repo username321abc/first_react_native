be7e5bd5d2620b345f034d0d726a868ac4984071
Your Name <you@example.com>
2026-02-14 19:16:38 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+  title="Learn More"
+  backgroundColor="#848484"
+  onPress={() => alert("eee")}
+  accessibilityLabel="Learn more about this purple button"
+/>
     </View>
   );
 }
